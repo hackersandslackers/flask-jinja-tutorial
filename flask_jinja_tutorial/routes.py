9f7c145d20758dcb37f6bafb01b8c21fd195d680
vishu21-at-710976054172 <vishu21-at-710976054172>
2020-07-05 12:55:08 +0530

    Inserting data class for Links - #1

--- flask_jinja_tutorial/routes.py
+++ flask_jinja_tutorial/routes.py
@@ -1,14 +1,24 @@
 """Route declaration."""
+from dataclasses import dataclass
+
 from flask import current_app as app
 from flask import render_template
+
+
+@dataclass
+class Link:
+    url: str
+    name: str
 
 
 @app.route('/')
 def home():
     """Landing page."""
-    nav = [{'name': 'Home', 'url': 'https://example.com/1'},
-           {'name': 'About', 'url': 'https://example.com/2'},
-           {'name': 'Pics', 'url': 'https://example.com/3'}]
+    nav = [
+        Link(name='Home', url='https://example.com/1'),
+        Link(name='About', url='https://example.com/2'),
+        Link(name='Pics', url='https://example.com/2')
+    ]
     return render_template('home.html',
                            nav=nav,
                            title="Jinja Demo Site",
